fc0be73b8b1a8bd64d87043fff96a6cf59a0282c
Neto test <Levschina_@mail.ru>
2023-05-28 23:41:08 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calqulate(int price) {
+        return price / 20;
+    }
+
+    public int calculate(int price) {
+        return price / 20;
+    }
+}
