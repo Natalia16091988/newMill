eb20eea420cf9566b3cfc49fe6962644863697fc
Neto test <Levschina_@mail.ru>
2023-06-08 20:52:05 +0300

    pravka

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,4 @@
 public class BonusMilesService {
-
-    public int calqulate(int price) {
-        return price / 20;
-    }
-
     public int calculate(int price) {
         return price / 20;
     }
